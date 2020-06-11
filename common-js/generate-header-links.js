f3b62c98f08ed7b4bda71cbd480faa002e7ccc08
sysc0 <tiix.gaming@gmail.com>
2020-06-11 08:50:06 +0200

    Update generate-header-links.js

--- common-js/generate-header-links.js
+++ common-js/generate-header-links.js
@@ -3,42 +3,16 @@ div.className = "header-links-div";
 
 // GitHub
 var githubLink = document.createElement("a");
-githubLink.href = "https://github.com/alexguirre";
+githubLink.href = "https://github.com/sysc0";
 githubLink.title = "GitHub";
-
-var githubImage = document.createElement("img");
-githubImage.style.float = "right";
-githubImage.src = document.location.origin + "/alexguirre.github.io/images/github.png";
-githubImage.width = 32;
-githubImage.height = 32;
-githubImage.style.marginRight = "0.25em";
-githubImage.style.marginLeft = "0.25em";
-
-githubLink.appendChild(githubImage);
-
-
-// YT
-var youtubeLink = document.createElement("a");
-youtubeLink.href = "https://www.youtube.com/channel/UCJxYR4tBSP6dP9fJica6uuw";
-youtubeLink.title = "YouTube";
-
-var youtubeImage = document.createElement("img");
-youtubeImage.style.float = "right";
-youtubeImage.src = document.location.origin + "/alexguirre.github.io/images/youtube.png";
-youtubeImage.width = 32;
-youtubeImage.height = 32;
-youtubeImage.style.marginLeft = "0.25em";
-
-youtubeLink.appendChild(youtubeImage);
-
 
 // Home
 var homeText = document.createElement("h1");
 
 var homeLink = document.createElement("a");
-homeLink.href = document.location.origin + "/alexguirre.github.io";
+homeLink.href = document.location.origin + "/sysc0.github.io";
 homeLink.className = "header-link";
-homeLink.textContent = "alexguirre";
+homeLink.textContent = "Fivem Personnal Resources";
 
 homeText.appendChild(homeLink);
 
